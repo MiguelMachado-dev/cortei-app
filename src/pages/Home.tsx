57caaf12eced9b81f4d1f219571f0282631f5b2d
Miguel Machado <hello@miguelmachado.dev>
2025-10-17 23:39:27 -0300

    feat: base project structure

---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import Input from "../components/Input";
+
+const Home = () => {
+  return (
+    <div className="font-catamaran antialiased bg-gray-800 min-h-screen flex flex-col items-center justify-center gap-4">
+      <p>test</p>
+      <Input />
+    </div>
+  );
+};
+
+export default Home;
