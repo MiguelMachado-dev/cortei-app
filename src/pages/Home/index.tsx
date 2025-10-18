c9b01abdf4cbddcbd4e386afa3949a87a1c77582
Miguel Machado <hello@miguelmachado.dev>
2025-10-18 19:10:34 -0300

    feat: add TextInput component and Home page with basic layout

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,12 @@
+import TextInput from "../../components/TextInput";
+
+const Home = () => {
+  return (
+    <div className="font-catamaran flex min-h-screen flex-col items-center justify-center gap-4 bg-gray-800 antialiased">
+      <p>test</p>
+      <TextInput />
+    </div>
+  );
+};
+
+export default Home;
