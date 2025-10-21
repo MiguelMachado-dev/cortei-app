2c63eeaab1ca7905d4b1caf4cb3c9cba3075ad9d
Miguel Machado <hello@miguelmachado.dev>
2025-10-21 15:23:54 -0300

    refactor: update DatePicker with label, state management, and validation

--- src/components/DatePicker.tsx
+++ src/components/DatePicker.tsx
@@ -1,35 +1,58 @@
-import { CalendarIcon } from "@phosphor-icons/react";
-import { format } from "date-fns";
-
 import { Button } from "@/components/ui/button";
 import { Calendar } from "@/components/ui/calendar";
+import { Label } from "@/components/ui/label";
 import {
   Popover,
   PopoverContent,
   PopoverTrigger,
 } from "@/components/ui/popover";
+import { CalendarIcon, ChevronDownIcon } from "lucide-react";
+import { useState } from "react";
 
 interface DatePickerProps {
   date: Date | undefined;
-  setDate: (value: Date | undefined) => void;
+  setDate: (date: Date | undefined) => void;
 }
 
-export function DatePickerDemo({ date, setDate }: DatePickerProps) {
+export function DatePicker({ date, setDate }: DatePickerProps) {
+  const [open, setOpen] = useState(false);
+  const today = new Date();
+  today.setHours(0, 0, 0, 0);
+
   return (
-    <Popover>
-      <PopoverTrigger asChild>
-        <Button
-          variant="outline"
-          data-empty={!date}
-          className="data-[empty=true]:text-muted-foreground w-[280px] justify-start text-left font-normal"
-        >
-          <CalendarIcon />
-          {date ? format(date, "PPP") : <span>Pick a date</span>}
-        </Button>
-      </PopoverTrigger>
-      <PopoverContent className="w-auto p-0">
-        <Calendar mode="single" selected={date} onSelect={setDate} />
-      </PopoverContent>
-    </Popover>
+    <div className="flex flex-col gap-2">
+      <Label htmlFor="date" className="text-base font-bold text-gray-200">
+        Data
+      </Label>
+      <Popover open={open} onOpenChange={setOpen}>
+        <PopoverTrigger asChild>
+          <Button
+            variant="ghost"
+            id="date"
+            className="w-48 items-center justify-between border border-gray-500 font-normal"
+          >
+            <div className="flex items-center gap-2 text-[color:var(--yellow)]">
+              <CalendarIcon width={16} height={16} />
+              <p className="self-baseline-last text-sm leading-none text-gray-200">
+                {date ? date.toLocaleDateString() : "selecione uma data"}
+              </p>
+            </div>
+            <ChevronDownIcon width={16} height={16} />
+          </Button>
+        </PopoverTrigger>
+        <PopoverContent className="w-auto overflow-hidden p-0" align="start">
+          <Calendar
+            mode="single"
+            selected={date}
+            captionLayout="dropdown"
+            disabled={{ before: today }}
+            onSelect={(date) => {
+              setDate(date);
+              setOpen(false);
+            }}
+          />
+        </PopoverContent>
+      </Popover>
+    </div>
   );
 }
